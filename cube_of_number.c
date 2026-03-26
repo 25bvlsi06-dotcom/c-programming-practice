313356e4da1b3b070774a966419ce01644bd747f
Vibhay Kumar <265566916+25bvlsi06-dotcom@users.noreply.github.com>
2026-03-26 15:52:49 +0530

    create cube of the number.c

---
+++ cube_of_number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a, cube;
+    printf("enter the number:");
+    scanf("%d",&a);
+    cube=a*a*a;
+    printf("the cube of the number:%d",cube);
+
+    
+    return 0;
+}
